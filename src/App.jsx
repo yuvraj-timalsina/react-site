393270b00e1db85f1a62f1bca83ae6df9d5884b0
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-06-22 12:41:55 +0545

    Custom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Page from "./components/Page.jsx";
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-          <h1>Fun facts about React</h1>
-          <ul>
-              <li>Released in 2013</li>
-              <li>Created by Jordan Walke</li>
-              <li>Over 100K stars in GitHub</li>
-              <li>Maintained by Facebook</li>
-              <li>Powers thousands of Enterprise apps & Mobile Apps</li>
-          </ul>
-      </div>
+     <Page/>
     </>
   )
 }
