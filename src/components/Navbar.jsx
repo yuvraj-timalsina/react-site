1aaece5ec35e647bab4aa18b2dbfdf4a69b35b40
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-06-27 12:58:22 +0545

    main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
             <nav className="nav">
                 <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo" alt="React Logo"/>
+                    <img src={reactLogo}     alt="React Logo" className="nav-icon"/>
                 </a>
                 <h3 className="nav-logo-text">React Facts</h3>
                 <h4 className="nav-title">React Course - Project 1</h4>
