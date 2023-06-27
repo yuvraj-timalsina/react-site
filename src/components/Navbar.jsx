06d3bfa1749c9bda106b5759052d485e942a63c0
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-06-27 12:19:52 +0545

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ import reactLogo from "../assets/react.svg";
 
 function Navbar() {
     return (
-        <h1>Navbar</h1>
+            <nav className="nav">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                    <img src={reactLogo} className="logo" alt="React Logo"/>
+                </a>
+                <h3 className="nav-logo-text">React Facts</h3>
+                <h4 className="nav-title">React Course - Project 1</h4>
+            </nav>
     );
 }
 
