b65618cf36b1c28423bf80601c1afcec2421c3c1
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-06-27 13:12:36 +0545

    Background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
                     <img src={reactLogo}     alt="React Logo" className="nav-icon"/>
                 </a>
                 <h3 className="nav-logo-text">React Facts</h3>
-                <h4 className="nav-title">React Course - Project 1</h4>
+                <h4 className="nav-title">React Course - Static Site</h4>
             </nav>
     );
 }
